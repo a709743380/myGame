844ddbb55c7fd2034768861a3383ebdb48ec06b8
Ian <a709743380@gmail.com>
2026-06-09 19:41:14 +0800

    test commit

--- ConsoleApp2/Games/MoraGame.cs
+++ ConsoleApp2/Games/MoraGame.cs
@@ -69,6 +69,7 @@ namespace ConsoleApp2.Games
             } while (running);
 
         }
+
         /// <summary>
         /// 初始化
         /// </summary>
--- ConsoleApp2/Services/GameServices.cs
+++ ConsoleApp2/Services/GameServices.cs
@@ -10,10 +10,10 @@ namespace ConsoleApp2.Services
 {
     public class GameAutoFac
     {
-        private static IContainer _container;
+        private static  IContainer? _container;
 
         // 在建構子中建立容器並註冊服務
-        public  static void Register()
+        public static void Register()
         {
             // 1. 建立容器建構器
             var builder = new ContainerBuilder();
@@ -24,13 +24,17 @@ namespace ConsoleApp2.Services
             // 取得已註冊的服務
             var registrations = _container.ComponentRegistry.Registrations;
         }
-
+        /// <summary>
+        /// 取得遊戲內容
+        /// </summary>
+        /// <param name="GameType"></param>
+        /// <returns></returns>
         public bool PlayGame(GameList GameType)
         {
             // 使用先前建立的容器
-            using (var scope = _container.BeginLifetimeScope())
+            using (var scope = _container?.BeginLifetimeScope())
             {
-                var gameTasks = scope.Resolve<IEnumerable<Meta<IGameTask>>>();
+                var gameTasks = scope?.Resolve<IEnumerable<Meta<IGameTask>>>();
                 var lazy = gameTasks
                     ?.FirstOrDefault(t => GameType.Equals(t.Metadata[nameof(GameAttribute.Type)]))
                     ?.Value;
@@ -39,28 +43,5 @@ namespace ConsoleApp2.Services
                 return lazy.Regame;
             }
         }
-
-        //這樣每次都要new一個太奇怪了
-        //public void PlayGame(GameList GameType)
-        //{
-        //    // 1. 建立容器建構器
-        //    var builder = new ContainerBuilder();
-        //    // 2. 註冊服務
-        //    builder.RegisterModule<GameModule>();
-        //    // 建立容器
-        //    IContainer container = builder.Build();
-        //    // 4. 解析依賴項
-        //    using (var scope = container.BeginLifetimeScope())
-        //    {
-        //        ////檢查是否注冊成功
-        //        //bool isRegistered = container.IsRegistered<IGameTask>(); 
-        //        var gameTasks = scope.Resolve<IEnumerable<Meta<IGameTask>>>();
-        //        //取得實際游戲
-        //        var lazy = gameTasks
-        //            ?.FirstOrDefault(t => GameType.Equals(t.Metadata[nameof(GameAttribute.Type)]))
-        //            ?.Value;
-        //        lazy.Execute();
-        //    }
-        //}
     }
 }
